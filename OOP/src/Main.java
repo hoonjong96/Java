3a852e5cef7c10edf2413be62c588b5839890a1c
Jongdroid <qkr7627@gmail.com>
2022-12-31 15:36:11 +0900

    first commit

---
+++ OOP/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author jongdroid
+ * @email qkr7627@gmail.com
+ * @created ${DATE}
+ * @desc
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
