4b05be1dc2ef80bd5d8593f5fe2419b309055636
Jongdroid <qkr7627@gmail.com>
2023-01-01 22:45:24 +0900

    Create: Cat class, meow method for study instance

---
+++ OOP/src/Cat.java
@@ -0,0 +1,18 @@
+/**
+ * @author jongdroid
+ * @email qkr7627@gmail.com
+ * @created 2023/01/01
+ * @desc
+ */
+
+// 클래스를 통해 생성된 객체를 인스턴스라고 하며
+// 클래스의 메소드를 호출하는 것을 인스턴스 메소드를 호출한다고 표현한다.
+public class Cat {
+
+    String name;
+    int age;
+
+    void meow() {
+        System.out.println("고양이는 야옹~~ 하고 울지요");
+    }
+}
