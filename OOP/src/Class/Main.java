151c1d29291f5768e7c8c96601259ae7c56d5e49
Jongdroid <qkr7627@gmail.com>
2023-01-05 21:46:37 +0900

    Constructor 실습 - Notebook

--- OOP/src/Class/Main.java
+++ OOP/src/Class/Main.java
@@ -1,3 +1,7 @@
+package Class;
+
+import Constructor.Notebook;
+
 /**
  * @author jongdroid
  * @email qkr7627@gmail.com
@@ -10,7 +14,7 @@ public class Main {
 
         // dog 객체 생성(인스턴스)
         // new 키워드를 사용한다.
-        // 왼쪽의 Dog dog는 Dog타입의 변수를 의미한다.
+        // 왼쪽의 Class.Dog dog는 Dog타입의 변수를 의미한다.
         Dog dog = new Dog();
 
         // dog 객체의 필드를 출력한다. 하지만 값이 없기 때문에 null, 0 이 나올 것이다.
@@ -29,12 +33,12 @@ public class Main {
         System.out.printf("강아지의 이름: %s\n", dog.name);
         System.out.printf("루루의 나이: %d\n", dog.age);
 
-        // 목표 : Cat 클래스의 인스턴스 메소드를 호출해봅시다.
+        // 목표 : Class.Cat 클래스의 인스턴스 메소드를 호출해봅시다.
 
-        // 우선 Cat 클래스를 객체로 만든다.
+        // 우선 Class.Cat 클래스를 객체로 만든다.
         Cat cat = new Cat();
 
-        // 필드 호출과 동일하게 도트연산자를 통해 Cat 클래스에 있는 meow 메소드 내부를 실행한다.
+        // 필드 호출과 동일하게 도트연산자를 통해 Class.Cat 클래스에 있는 meow 메소드 내부를 실행한다.
         cat.meow();
 
 
@@ -58,8 +62,18 @@ public class Main {
         s1.length = 4;
 
         // 바로 출력해보자
-        System.out.printf("한 변의 길이가 %d인 정사각형의 넓이: %d", s1.length, s1.area());
+        System.out.printf("한 변의 길이가 %d인 정사각형의 넓이: %d\n", s1.length, s1.area());
 
+        // 실습 Constructor
+        // 객체를 만들면서 초기화도 같이 해보자
+        Notebook macbook = new Notebook("맥북프로 m1 14형", 16.5, 13.3, "silver");
+
+        // 이번엔 인텔 노트북 만들어보자
+        Notebook intelNotebook = new Notebook("그냥인텔노트북", 19.9, 16.3, "black");
+
+        // 노트북 정보 출력
+        System.out.printf("저의 맥북의 이름은 %s 이고, 꽤 무거운 %f의 %f인치 이고 %s 색상입니다.\n", macbook.name, macbook.weight, macbook.displaySize, macbook.color);
+        System.out.printf("저의 인텔 노트북 이름은 %s 이고, 엄청 무거운 %f의 %f인치 이고 %s 색상입니다.\n", intelNotebook.name, intelNotebook.weight, intelNotebook.displaySize, intelNotebook.color);
 
     }
 }
---
+++ OOP/src/Constructor/Notebook.java
@@ -0,0 +1,23 @@
+package Constructor;
+
+/**
+ * @author jongdroid
+ * @email qkr7627@gmail.com
+ * @created 2023/01/05
+ * @desc
+ */
+public class Notebook {
+    // 필드를 만들어봅시다
+    public String name;
+    public Double weight;
+    public Double displaySize;
+    public String color;
+
+    public Notebook(String n, Double w, Double d, String c) {
+        name = n;
+        weight = w;
+        displaySize = d;
+        color = c;
+    }
+
+}
